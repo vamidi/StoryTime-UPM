a561460eaaf0b90358f5cb6b194d3e08fa03b28b
vamidi <valencio_masaki16@live.nl>
2023-01-13 16:18:22 +0100

    Release 2020.1.9

---
+++ Editor/VisualScripting/Elements/Dialogue/StartNode.cs
@@ -0,0 +1,26 @@
+﻿using StoryTime.Editor.VisualScripting.Utilities;
+using UnityEditor;
+using Node = StoryTime.VisualScripting.Data.ScriptableObjects.Node;
+namespace StoryTime.Editor.VisualScripting.Elements
+{
+	public class StartNode : NodeView
+	{
+		public StartNode(BaseGraphView graphView, Node node) : base(graphView, node) { }
+
+		public override void Draw()
+		{
+			base.Draw();
+
+			var portChoice = this.CreatePort("Next Dialogue");
+			output = portChoice;
+			outputContainer.Add(portChoice);
+
+			// keep track of undo records
+			Undo.undoRedoPerformed += OnUndoRedo;
+			RefreshExpandedState();
+			RefreshPorts();
+		}
+
+		private void OnUndoRedo() { }
+	}
+}
