347b6da1207052ccaed3da767fd8105f222534d1
Valencio Hoffman <valencio.hoffman@nsgo.nl>
2024-08-23 12:29:32 +0200

    - Major refactor and turning everything domains driven

---
+++ Runtime/Domains/Game/Characters/ScriptableObjects/Events/Enemies/EnemeyChannelSO.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace StoryTime.Domains.Game.Characters.ScriptableObjects.Events.Enemies
+{
+    using StoryTime.Domains.Game.NPC.Enemies.ScriptableObjects;
+	
+    [CreateAssetMenu(menuName = "StoryTime/Game/Events/Narrative/Enemy Channel")]
+    // ReSharper disable once InconsistentNaming
+    public class EnemyChannelSO : ScriptableObject
+    {
+        public UnityAction<EnemySO> OnEventRaised;
+        public void RaiseEvent(EnemySO enemySo)
+        {
+            OnEventRaised?.Invoke(enemySo);
+        }
+    }
+}
